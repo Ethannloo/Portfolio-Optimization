d73f21a0f9202ec3d34ec37455285e66cdb75f95
Ethan Loo <147348919+Ethannloo@users.noreply.github.com>
2025-04-29 19:24:27 -0700

    Update data.py

--- data.py
+++ data.py
@@ -4,12 +4,13 @@ import numpy as np
 
 tickers = ['AAPL', 'MSFT', 'GOOG', 'TSLA', 'NVDA', 'AMZN', 'META']
 
-d = yf.download(tickers, start='2020-01-01', end='2025-01-01', auto_adjust=False)['Adj Close']
+d = yf.download(tickers, start='2019-01-01', end='2025-01-01', auto_adjust=False)['Adj Close']
 monthly_d = d.resample('M').last()
 monthly_d.to_csv("data.csv")
-
 log_return = np.log(monthly_d / monthly_d.shift(1))
-log_return_matrix = log_return.dropna().to_numpy()
-
-print(log_return)
+log_return = log_return.dropna()
+mu = log_return.mean()
+mu_annualized = mu * 12
+sigma = log_return.cov()
+sigam_annual = sigma * 12
 
