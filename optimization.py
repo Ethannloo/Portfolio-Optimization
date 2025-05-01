a13b0d8d85338d9050729bc8272db530226ae814
Ethan Loo <147348919+Ethannloo@users.noreply.github.com>
2025-04-30 17:15:52 -0700

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -2,11 +2,26 @@ from data import *
 import numpy as np
 import cvxpy as cp
 
-mu_vector = mu.values
-sigma_matrix = sigma.values
+exp_returns = mu.values
+cov_matrix = sigma.values
 
-num = len(mu_vector)
+num = len(exp_returns)
 target = .01
+
+w = cp.Variable(num)
+portfolio_variance = cp.quad_form(w, cov_matrix)
+objective = cp.Minimize(portfolio_variance)
+
+
+contraints = [cp.sum(w)==1, exp_returns @ w >= target, w >= 0]
+
+problem = cp.Problem(objective, contraints)
+problem.solve()
+
+
+def portfolio_performance(weights, mean_returns, cov_matrix):
+    returns = np.sum(mean_returns*weights)*12
+    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(12)
 
 
 
